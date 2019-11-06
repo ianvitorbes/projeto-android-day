979b5ddbe605fd563b50beb4fad6c38120aee460
ianvitorbes <ianvitorbes@gmail.comgit config --global user.email ianvitorbes@gmail.comgit config --global user.email ianvitorbes@gmail.com>
2019-11-06 13:10:35 -0300

    Criação de toolbar, opções de menu

--- ParseStarterProject/src/main/java/com/parse/starter/Activitys/MainActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/Activitys/MainActivity.java
@@ -8,11 +8,14 @@
  */
 package com.parse.starter.Activitys;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.parse.FindCallback;
@@ -32,12 +35,54 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+  private Toolbar toolbarPrincipal;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
+      //Configuração da Toolbar
+
+      toolbarPrincipal = (Toolbar) findViewById(R.id.toolbar_principal);
+      setSupportActionBar( toolbarPrincipal );
 
   }
 
+    @Override
+
+    //criação de menu da toolbar
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+
+    }
+    //opções da toolbar
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_sair:
+                deslogarUsuario();
+                return true;
+
+            case R.id.action_configuracoes:
+                return true;
+            case R.id.action_compartilhar:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    private void deslogarUsuario() {
+        ParseUser.logOut();
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+
+
+    }
 }
