da7771328ee0372e476364d11aeb4d7570b64b93
ianvitorbes <ianvitorbes@gmail.com>
2019-11-15 18:12:08 -0300

    Metodo de postagem OK

--- ParseStarterProject/src/main/java/com/parse/starter/Activitys/MainActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/Activitys/MainActivity.java
@@ -9,7 +9,10 @@
 package com.parse.starter.Activitys;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
@@ -19,12 +22,14 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.GetCallback;
 import com.parse.LogInCallback;
 import com.parse.ParseAnalytics;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
@@ -34,6 +39,10 @@ import com.parse.starter.Adapter.TabsAdapter;
 import com.parse.starter.R;
 import com.parse.starter.util.SlidingTabLayout;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 
@@ -93,11 +102,85 @@ public class MainActivity extends AppCompatActivity {
             case R.id.action_configuracoes:
                 return true;
             case R.id.action_compartilhar:
+                compartilharFoto();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
 
         }
+    }
+
+    private void compartilharFoto(){
+
+        Intent intent = new Intent( Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI );
+        startActivityForResult(intent, 1);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        //Teste de retorno de dados
+        if( requestCode==1 && resultCode == RESULT_OK && data != null ){
+
+
+            //Recuperar local dos dados
+
+            Uri localImagemSelecionada = data.getData();
+
+            try {
+                Bitmap imagem = MediaStore.Images.Media.getBitmap( getContentResolver(), localImagemSelecionada );
+
+                //imagem em png
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                imagem.compress(Bitmap.CompressFormat.PNG, 75, stream);
+
+                //Array para converter o stream em array de bytes
+                byte[] byteArray = stream.toByteArray();
+
+
+                //Arquivo do mesmo formato do parse
+                SimpleDateFormat dateFormat = new SimpleDateFormat("ddmmaaaahhmmss");
+                String nomeFoto = dateFormat.format(new Date() );
+                ParseFile arquivoParse = new ParseFile( nomeFoto + "foto.png", byteArray );
+
+                //Obj pra salvar no parse
+
+                ParseObject parseObject = new ParseObject("Fotos");
+                parseObject.put("username", ParseUser.getCurrentUser().getUsername() );
+                parseObject.put("foto", arquivoParse );
+
+                //====//
+
+                parseObject.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+
+                        if( e==null ){
+                            Toast.makeText(getApplicationContext(), "A imagem foi postada!", Toast.LENGTH_SHORT).show();
+
+                        }else{
+                            Toast.makeText(getApplicationContext(), "Erro ao postar, tente novamente", Toast.LENGTH_SHORT).show();
+
+                        }
+
+
+
+                    }
+                });
+
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+
+            }
+
+        }
+
+
     }
 
     private void deslogarUsuario() {
